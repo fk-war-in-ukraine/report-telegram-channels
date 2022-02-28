449bed8750faef946f9a1cf4ca5319084a767ffb
Alexander Pevzner <a.pevzner@owox.com>
2022-02-28 09:27:34 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## Report dirty Telegram channels
+
+1. Установите Python 3 (при желании активируйте виртуальное окружение).
+2. Установите зависимости:
+```bash
+pip install -r requirements.txt
+```
+3. Получите `api_id` и `api_hash` (см. ниже).
+4. Поместите ссылки на каналы, которые надо отрепортить, в `dirty_channels.txt` (на каждую строку одну ссылку).
+5. Запустите из консоли скрипт в формате
+```bash
+python main.py <api_id> <api_hash>
+```
+При первом запуске скрипт спросит номер телефона и попросит ввести одноразовый код, который придет в Telegram
+для подтверждения аккаунта. При следующих запусках скрипт уже не будут ничего спрашивать.
+
+Пример правильной отработки скрипта:
+```bash
+$ python main.py 123456 827ccb0eea8a706c4c34a16891f84e7b
+Please enter your phone (or bot token): +380XXX
+Please enter the code you received: 123456
+Signed in successfully as Anonymous
+https://t.me/warjournaltg: True
+```
+
+###  Как получить `api_id` и `api_hash`
+1. Залогиньтесь в https://my.telegram.org.
+2. Перейдите в `API development tools` и заполните форму.
+3. Вы получите `api_id` и `api_hash`. В настоящий момент к одному номеру телефона может быть привязан только один `api_id`.
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import time
+from telethon.sync import TelegramClient
+from telethon import functions, types
+
+
+def print_help() -> None:
+    print('Usage:')
+    print('\tpython main.py <api_id> <api_hash>')
+
+
+def get_dirty_channels() -> list:
+    with open(os.path.join(os.path.dirname(os.path.relpath(__file__)), 'dirty_channels.txt'), 'r') as fp:
+        dirty_channels = fp.readlines()
+
+    return dirty_channels
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print_help()
+        exit(1)
+
+    api_id = int(sys.argv[1])
+    api_hash = sys.argv[2]
+
+    dirty_channels = get_dirty_channels()
+
+    with TelegramClient('ReportDirtyChannels', api_id, api_hash) as client:
+        for dirty_channel in dirty_channels:
+            dirty_channel = dirty_channel.strip()
+            try:
+                result = client(functions.account.ReportPeerRequest(
+                    peer=client.get_entity(dirty_channel),
+                    reason=types.InputReportReasonOther(),
+                    message='Русский корабль, иди нахуй!'
+                ))
+                print('{}: {}'.format(dirty_channel, result))
+                time.sleep(0.3)
+            except Exception as e:
+                print('{}: error'.format(dirty_channel))
